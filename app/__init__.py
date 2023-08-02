44a67b4ffd46b9650a88a1e7b71df865aaf98d6b
suraw00t <surawut.kong@gmail.com>
2023-08-02 15:05:31 +0700

    refactor: :bulb: update init app

--- app/__init__.py
+++ app/__init__.py
@@ -5,31 +5,34 @@ from app.core.config import get_app_settings
 from app.models import beanie_client
 
 
-def create_app() -> FastAPI:
-    settings = get_app_settings()
-    settings.configure_logging()
+class App:
+    def create_app(self) -> FastAPI:
+        settings = get_app_settings()
+        settings.configure_logging()
 
-    app = FastAPI(**settings.fastapi_kwargs)
+        self.app = FastAPI(**settings.fastapi_kwargs)
+        self.app.add_middleware(
+            CORSMiddleware,
+            allow_origins=settings.ALLOWED_HOSTS,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
 
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=settings.ALLOWED_HOSTS,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+        # @app.middleware("http")
+        # async def add_process_time_header(request: Request, call_next):
+        #     start_time = time.time()
+        #     response = await call_next(request)
+        #     process_time = time.time() - start_time
+        #     response.headers["X-Process-Time"] = str(process_time)
+        #     return response
 
-    # @app.middleware("http")
-    # async def add_process_time_header(request: Request, call_next):
-    #     start_time = time.time()
-    #     response = await call_next(request)
-    #     process_time = time.time() - start_time
-    #     response.headers["X-Process-Time"] = str(process_time)
-    #     return response
+        @self.app.on_event("startup")
+        async def init_app():
+            await init_router(self.app, settings)
+            await beanie_client.init_beanie(settings)
 
-    @app.on_event("startup")
-    async def init_app():
-        await init_router(app, settings)
-        await beanie_client.init_beanie(settings)
+        return self.app
 
-    return app
+
+fastapi = App()
